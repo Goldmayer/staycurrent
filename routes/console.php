b2b28acd5328cadf2c50e7a9706fc22efff6a53d
G01dmayer_91t <neoking.studio@gmail.com>
2026-02-06 16:13:10 +0700

    Configure Laravel scheduler: automate market sync, trade tick and trade close every 5 minutes

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,10 @@ Artisan::command('inspire', function () {
 
 // Register custom commands
 //require __DIR__.'/../app/Console/Commands/TradeTick.php';
+
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('market:sync')->everyFiveMinutes();
+Schedule::command('trade:tick')->everyFiveMinutes();
+Schedule::command('trade:close')->everyFiveMinutes();
+
